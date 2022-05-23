8230404e8a61296a206199eb98fb8bdd75e69f2b
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-23 09:43:57 +0100

    index.js created, package.json updated

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const { serverPort } = require('./dbConfig');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.send({ msg: 'Server is running' });
+});
+
+app.all('*', (req, res) => {
+  return res.status(404).send({ error: 'Page not found' });
+});
+
+app.listen(serverPort, () => {
+  console.log(`Server is running on port ${serverPort}`);
+});
