af2b14560c39a1d9fcae382702ceb7d0c7228697
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-24 12:48:21 +0300

    userSchema updated

--- src/models/auth.js
+++ src/models/auth.js
@@ -1,6 +1,9 @@
 const Joi = require('joi');
 
 const userSchema = Joi.object({
+  first_name: Joi.string(),
+  last_name: Joi.string(),
+  phone_number: Joi.number(),
   email: Joi.string().email().lowercase().trim().required(),
   password: Joi.string().required(),
 });
